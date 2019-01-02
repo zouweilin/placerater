a460a0d2605a4281f6b75eb03aa61eb884bb2921
Weilin Zou <weilinzou@gmail.com>
2019-01-01 22:56:09 -0500

    database stuff

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'CoreyMS',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'Date_posted': 'August 27,2018'
-    },
-    {
-        'author': 'CoreasdfS',
-        'title': 'Blog Post 2',
-        'content': '2nd post content',
-        'Date_posted': 'August 227,2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
